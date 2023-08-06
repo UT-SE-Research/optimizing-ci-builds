351890d095124c451e12073799969ac526bedc59
Shanto Rahman <shanto@ECE-A55006.austin.utexas.edu>
2023-08-06 13:47:43 -0500

    script update

--- automation_in_finding_plugin/static-approach/find_plugin_corpus.py
+++ automation_in_finding_plugin/static-approach/find_plugin_corpus.py
@@ -7,6 +7,7 @@ import json
 from nltk.tokenize import word_tokenize
 from nltk.stem import PorterStemmer
 from nltk.tokenize import regexp_tokenize
+import csv
 
 pomFile=sys.argv[1]
 unnecessary_directory=sys.argv[2]
@@ -16,6 +17,7 @@ tree = ET.parse(pomFile)
 
 # Get the root element of the XML file
 root = tree.getroot()
+key_list=[]
 plugin_corpora_dict = {}
 stop_word=["maven",""]
 # if the element is found, traverse its children and descendants
@@ -39,7 +41,7 @@ if target_element is not None:
                         #print('child3.tag='+child3.tag.split("}")[1])
                         level_count +=1
                         if level_count <= 3:
-                            print(child3.tag)
+                            #print(child3.tag)
                             if child3.tag.split("}")[1] == "artifactId" or child3.tag.split("}")[1] == "groupId":
                                 if key == "":
                                      key=child3.text
@@ -56,36 +58,59 @@ if target_element is not None:
                             corpora_list.append(child3.text)               
                     #key="plugin"+str(plugin_count)
                     plugin_corpora_dict[key]=corpora_list
+                    #print(key + ",corpora_list"+ str(corpora_list))
+                    #print("keys="+key)
+                    key_list.append(key)
+
+
                     #print(corpora_list)
 plugin_corpora_dict["empty"]=[]
-#for key, value in plugin_corpora_dict.items():
-#    print(key, value)
+
+'''with open('Unused_dir_And_all_tried_plugins.csv', 'a', newline='') as file:
+    writer = csv.writer(file)
+    writer.writerow(["Proj_name","Workflow","Unused-Dir-Name", "Plugin-That-Are-Used-For-Tf-IDF"])  # Write header
+    writer.writerow([sys.argv[3],sys.argv[4],unnecessary_directory, key_list])'''
 
 unnecessary_list_corpora=unnecessary_directory.split("/")
+vectorizer = TfidfVectorizer()
 
-# filter out non-string objects and convert to lowercase
-#unnecessary_list_corpora = [word.lower() for word in unnecessary_list_corpora if isinstance(word, str)]
-#plugin_corpora_dict = {key: [word.lower() for word in value if isinstance(word, str)] for key, value in plugin_corpora_dict.items()}
+result_plugin="Result_with_only_plugin_name.csv"
+result_plugin_arr=[]
+for plugin in key_list:
+    if len(plugin) >=1 and len(unnecessary_list_corpora) >=1:
+        #tfidf_list1 = vectorizer.transform(unnecessary_list_corpora)
+        vectorizer.fit(unnecessary_list_corpora + [plugin])
+        tfidf_list1 = vectorizer.transform(unnecessary_list_corpora)
+        tfidf_list2 = vectorizer.transform([plugin])
+        cosine_sim = cosine_similarity(tfidf_list1, tfidf_list2)
+        local_max_sim = np.max(cosine_sim)
+        print('****plugin=',plugin,',unnecessary_directory=', unnecessary_directory ,',score=',local_max_sim)
+        if local_max_sim > 0.0:
+            result_plugin_arr.append(plugin+':'+str(round(local_max_sim,3)))
 
-###SIMILARITY SCORE CALCULATE
-max_sim=0.0
-non_zero_matched_plugin_with_unused_dict={}
+with open(result_plugin, 'a', newline='') as file:
+    writer = csv.writer(file)
+    writer.writerow(["Proj_name","Workflow","Unused-Dir-Name", "Plugin-Name"])  # Write header
+    writer.writerow([sys.argv[3],sys.argv[4],unnecessary_directory, result_plugin_arr])
+
+###SIMILARITY SCORE CALCULATE Based off of the full plugin info
+'''non_zero_matched_plugin_with_unused_dict={}
 plugin=""
-for key, value in plugin_corpora_dict.items():
-    vectorizer = TfidfVectorizer()
-    vectorizer.fit(unnecessary_list_corpora + value)
+for key, plugin in plugin_corpora_dict.items():
+    #print('****Value='+str(value))
+    vectorizer.fit(unnecessary_list_corpora + plugin)
     # Transform the lists into TF-IDF vectors
     #print(len(value))
-    if len(value) >=1 and len(unnecessary_list_corpora) :
+    if len(plugin) >=1 and len(unnecessary_list_corpora) :
         tfidf_list1 = vectorizer.transform(unnecessary_list_corpora)
-        tfidf_list2 = vectorizer.transform(value)
+        tfidf_list2 = vectorizer.transform(plugin)
         cosine_sim = cosine_similarity(tfidf_list1, tfidf_list2)
         #print(cosine_sim)
         local_max_sim = np.max(cosine_sim)
         if local_max_sim > 0.0:
             non_zero_matched_plugin_with_unused_dict[key]=round(local_max_sim,3)
 sorted_dict=dict(sorted(non_zero_matched_plugin_with_unused_dict.items(), key=lambda x:x[1],reverse=True))
-with open('Result.csv', 'a') as file:
+with open('Result_1.csv', 'a') as file:
     #f.write(sorted_dir)
-    json.dump(sorted_dict, file)
+    json.dump(sorted_dict, file)'''
 #print(sorted_dict)
